e96720a9f9ac3c9119e361a934d928029eb271e2
alphamupi <78317530+RheaAgarwal1106@users.noreply.github.com>
2022-12-07 19:23:02 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write("""
+# TDS Module 8 Graded Assignment
+## For the Multiplication of 2 given numbers using Streamlit and deploying on Heroku
+""")
+#Input the numbers
+first_no = st.number_input("Enter first number")
+second_no = st.number_input("Enter second number")
+product = first_no*second_no
+st.write("The product of given numbers is", str(product))
+st.write("By Rhea Agarwal (21f1003085)")
